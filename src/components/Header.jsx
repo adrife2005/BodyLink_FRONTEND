c9e6570d627ef6ebea086a55f1287da60a48eb25
c0v4m3rl0 <acovarrubias17@ucol.mx>
2024-08-08 11:59:01 -0600

    chore: Finish the preofesional form with all the authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,16 @@ import { LuSunMedium } from "react-icons/lu";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import '../css/components/header.css'
 import Sidebar from "./Sidebar";
+import { useState } from "react";
 
 const Header = () => {
+const [openTheme, setOpenTheme] = useState(false)
+
+  const changeTheme = () => {
+    setOpenTheme(true)
+  }
+
+
   return (
     <>
     <header className="header">
@@ -12,11 +20,11 @@ const Header = () => {
           <img src="./BodyLink.png" alt="" />
         </div>
         <div className="header__profile">
-          <span className="header__profile__theme">
+          <span className="header__profile__theme" onClick={changeTheme}>
             <LuSunMedium  />
           </span>
           <span className="header__profile__theme">
-          <IoIosNotificationsOutline />
+            <IoIosNotificationsOutline />
           </span>
           <span className="header__profile__line"></span>
           <div className="header__profile__config">
