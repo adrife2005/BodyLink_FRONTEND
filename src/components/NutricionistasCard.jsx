04f18d58139045942138b29337a787b3e1e7b5da
c0v4m3rl0 <acovarrubias17@ucol.mx>
2024-08-14 17:48:36 -0600

    chore: finish making the cards

--- src/components/NutricionistasCard.jsx
+++ src/components/NutricionistasCard.jsx
@@ -1,8 +1,12 @@
 import '../css/components/nutricionistascard.css'
 
-const NutricionistasCard = () => {
+const NutricionistasCard = ({data}) => {
   return (
-    <div>NutricionistasCard</div>
+    <div className='nutricionistas__card'>
+      <div className="nutricionista__card__img">
+        <img src="https://m.media-amazon.com/images/I/71VaQ+V6XnL._AC_UY695_.jpg" alt="Profesional" />
+      </div>
+    </div>
   )
 }
 
