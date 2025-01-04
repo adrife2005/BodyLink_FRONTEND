89599df2ed9328bc73539a71b289fa7233b6d635
AdrifeCova <adrife2000@hotmail.com>
2025-01-04 16:15:39 -0600

    feat: sidebar fixed

--- src/components/_shared/sidebar/Sidebar.tsx
+++ src/components/_shared/sidebar/Sidebar.tsx
@@ -1,35 +1,37 @@
-import { SidebarItem } from './components/SidebarItem';
-import { useLocation } from 'react-router-dom';
+import { SidebarItem } from './components/SidebarItem'
+import { useLocation } from 'react-router-dom'
 
-import styles from './Sidebar.module.css';
-import items from './SidebarMenuItems.json';
-import getIcon from '../../../utils/getIcons';
+import styles from './Sidebar.module.css'
+import items from './SidebarMenuItems.json'
+import getIcon from '../../../utils/getIcons'
 
 export function Sidebar() {
-  const location = useLocation();
-  const isActive = (path: string): boolean => location.pathname === path;
+	const location = useLocation()
+	const isActive = (path: string): boolean => location.pathname === path
 
-  return (
-    <div data-testid='sidebard' className={styles.sidebar}>
-      <div className={styles.sidebarWrapper}>
-        <div>
-          <h1 className={styles.logo}>
-            BD <span>Body Link</span>
-          </h1>
-        </div>
-        <div>
-          {items.map((item, index) => (
-            <SidebarItem
-              active={isActive(item.path)}
-              key={index}
-              href={item.path}
-            >
-              {getIcon(item.icon)}
-              {item.title}
-            </SidebarItem>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div data-testid="sidebard" className={styles.sidebar}>
+			<div className={styles.sidebarContent}>
+				<div className={styles.sidebarWrapper}>
+					<div>
+						<h1 className={styles.logo}>
+							BD <span>Body Link</span>
+						</h1>
+					</div>
+					<div>
+						{items.map((item, index) => (
+							<SidebarItem
+								active={isActive(item.path)}
+								key={index}
+								href={item.path}
+							>
+								{getIcon(item.icon)}
+								{item.title}
+							</SidebarItem>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
