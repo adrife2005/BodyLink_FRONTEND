3f132e60a404e754f0d9dc0d053bf249e0ad0fe5
AdrifeCova <adrife2000@hotmail.com>
2025-01-04 13:32:17 -0600

    refactor: used customButton

--- src/components/_shared/header/Header.tsx
+++ src/components/_shared/header/Header.tsx
@@ -1,29 +1,31 @@
-import styles from './Header.module.css';
-import { Sun, Bell, ChevronDown, ChevronRight } from 'lucide-react';
-import { useLocation } from 'react-router-dom';
+import styles from './Header.module.css'
+import { Sun, Bell, ChevronDown, ChevronRight } from 'lucide-react'
+import { useLocation } from 'react-router-dom'
+import { CustomButton } from '../../ui'
+import { btnPrimaryStyles } from '../../ui/button/customStyles/buttonStyles'
 
 export function Header() {
-  const location = useLocation();
-  const locationName = location.pathname.replace('/', '');
-  const formatingLocation = locationName
-    .charAt(0)
-    .toUpperCase()
-    .concat(locationName.slice(1));
+	const location = useLocation()
+	const locationName = location.pathname.replace('/', '')
+	const formatingLocation = locationName
+		.charAt(0)
+		.toUpperCase()
+		.concat(locationName.slice(1))
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        {/* This will change */}
-        Inicio <ChevronRight /> {locationName && `${formatingLocation}`}
-      </div>
-      <div className={styles.right}>
-        <Sun />
-        <Bell />
-        <button type='button'>
-          Dr. Carlos
-          <ChevronDown />
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.left}>
+				{/* This will change */}
+				Inicio <ChevronRight /> {locationName && `${formatingLocation}`}
+			</div>
+			<div className={styles.right}>
+				<Sun />
+				<Bell />
+				<CustomButton className={btnPrimaryStyles}>
+					Dr. Carlos
+					<ChevronDown />
+				</CustomButton>
+			</div>
+		</div>
+	)
 }
