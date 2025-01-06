5f51e9b28c1728a8f5ce7be7a753fc7854955d9b
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-01-06 05:33:35 -0400

    feat: added the search bar to pacients page

--- src/pages/pacients/Pacients.tsx
+++ src/pages/pacients/Pacients.tsx
@@ -1,5 +1,8 @@
 import { SectionHeader } from '@/components/_shared/sectionHeader/SectionHeader'
-import { UsersRound } from 'lucide-react'
+import { Label } from '@/components/ui/label/Label'
+import { UsersRound, Search, SlidersHorizontal } from 'lucide-react'
+import styles from './Pacients.module.css'
+import labels from './filter-labels.json'
 
 export function Pacients() {
   return (
@@ -9,6 +12,23 @@ export function Pacients() {
         title='Pacientes'
         description='Registra, administra y archiva perfiles'
       />
+      <section className={styles['search-section']}>
+        <div className={styles.searchBar}>
+          <div>
+            <Search />
+          </div>
+          <input type='text' placeholder='Busca clientes, Archivos, Etc...' />
+        </div>
+        <div className={styles.filter}>
+          <SlidersHorizontal />
+        </div>
+        <p>354 resultados</p>
+      </section>
+      <section className={styles['filter-labels']}>
+        {labels.map(label => (
+          <Label text={label.text} color={label.color} icon={label.icon} />
+        ))}
+      </section>
     </>
   )
 }
