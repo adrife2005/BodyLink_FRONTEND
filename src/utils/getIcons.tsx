7c250f9ee3cb0ab65b160afc92034bdfe56d3aec
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-01-06 15:39:07 -0400

    refactor: getIcons is more dynamic, accepting props

--- src/utils/getIcons.tsx
+++ src/utils/getIcons.tsx
@@ -40,6 +40,7 @@ const icons: Icons = {
   MessageSquare,
   Newspaper,
   X,
+<<<<<<< HEAD
   Search,
   UserRoundPen,
   Calendar,
@@ -61,4 +62,12 @@ export const getIcon = (iconName: string, props?: Props): ReactElement => {
   const SVGComp = icons[iconName]
 
   return SVGComp && <SVGComp aria-label={iconName} {...props} />
+=======
+}
+
+export const getIcon = (icon: string, props?: Props): ReactElement => {
+  const SVGComp = icons[icon]
+
+  return SVGComp && <SVGComp {...props} />
+>>>>>>> 1927a10 (refactor: getIcons is more dynamic, accepting props)
 }
