257836b141976f16cbf9720906acff174ca628a6
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-01-06 22:10:56 -0400

    feat: added register modal tests

--- src/components/_shared/modal/GenericModal.tsx
+++ src/components/_shared/modal/GenericModal.tsx
@@ -14,6 +14,7 @@ export default function GenericModal({ title, children, blur, close }: Props) {
     <>
       {blur && <div className={styles.blur}></div>}
 <<<<<<< HEAD
+<<<<<<< HEAD
       <div data-testid='genericModal' className={styles['generic-modal']}>
         <div>
           <h1> {title}</h1>
@@ -24,6 +25,12 @@ export default function GenericModal({ title, children, blur, close }: Props) {
           <h1> {title}</h1>
           <X onClick={close} />
 >>>>>>> 3bdae39 (feat: added generic modal and register modal)
+=======
+      <div data-testid='genericModal' className={styles['generic-modal']}>
+        <div>
+          <h1> {title}</h1>
+          <X data-testid='modalCloseButton' onClick={close} />
+>>>>>>> 0921ba3 (feat: added register modal tests)
         </div>
         <div>{children}</div>
       </div>
