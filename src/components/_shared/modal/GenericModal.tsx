3bdae39ec4e3a77eec5f2ed038c663741d18c297
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-01-06 21:31:25 -0400

    feat: added generic modal and register modal

---
+++ src/components/_shared/modal/GenericModal.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+import styles from './GenericModal.module.css'
+import { X } from 'lucide-react'
+
+interface Props {
+  title: string
+  children: ReactNode
+  blur: boolean
+  close: () => void
+}
+
+export default function GenericModal({ title, children, blur, close }: Props) {
+  return (
+    <>
+      {blur && <div className={styles.blur}></div>}
+      <div className={styles['generic-modal']}>
+        <div>
+          <h1> {title}</h1>
+          <X onClick={close} />
+        </div>
+        <div>{children}</div>
+      </div>
+    </>
+  )
+}
