19f7226f2ff5e6bb46d66f497e67b90f970474dd
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-01-09 20:50:13 -0400

    feat: added the patients table

--- src/pages/patients/Patients.tsx
+++ src/pages/patients/Patients.tsx
@@ -5,9 +5,17 @@ import { Input } from '@/components/ui/input/Input'
 import { CustomButton } from '@/components/ui/button/CustomButton'
 import { btnPrimaryStyles } from '@/components/ui/button/customStyles/buttonStyles'
 
-import { UsersRound, SlidersHorizontal, Plus } from 'lucide-react'
+import {
+  UsersRound,
+  SlidersHorizontal,
+  Plus,
+  ArrowUpDown,
+  Grid2x2,
+  CircleUser,
+} from 'lucide-react'
 import styles from './Patients.module.css'
 import labels from './filter-labels.json'
+import patients from './patients-table.json'
 
 import { useState } from 'react'
 import { createPortal } from 'react-dom'
@@ -55,7 +63,7 @@ export function Patients() {
         ))}
       </section>
       <section data-testid='patients-data' className={styles['patients-data']}>
-        <main className={styles['patients-table']}>
+        <main className={styles['patients-selection']}>
           <div>
             <button
               className={`${activeView === 'Activos' && styles.isActive}`}
@@ -70,7 +78,72 @@ export function Patients() {
               Archivados
             </button>
           </div>
+          <div>
+            <div>
+              <div>
+                <span>Vista por pagina</span>
+                <select defaultValue='6'>
+                  <option value='1'>1</option>
+                  <option value='1'>2</option>
+                  <option value='1'>3</option>
+                  <option value='1'>4</option>
+                  <option value='1'>5</option>
+                  <option value='1'>6</option>
+                  <option value='1'>7</option>
+                  <option value='1'>8</option>
+                  <option value='1'>9</option>
+                  <option value='1'>10</option>
+                </select>
+              </div>
+              <Grid2x2 />
+            </div>
+            <table className={styles['patients-table']}>
+              <thead>
+                <tr>
+                  <td>
+                    Paciente
+                    <ArrowUpDown />
+                  </td>
+                  <td>
+                    e-Mail
+                    <ArrowUpDown />
+                  </td>
+                  <td>
+                    Ultima consulta
+                    <ArrowUpDown />
+                  </td>
+                  <td>
+                    Objetivo
+                    <ArrowUpDown />
+                  </td>
+                  <td>Acción</td>
+                </tr>
+              </thead>
+              <tbody>
+                {patients.map(patient => (
+                  <tr key={patient.objective}>
+                    <td>
+                      <CircleUser color='#19a853' />
+                      {patient.name}
+                    </td>
+                    <td>{patient.email}</td>
+                    <td>{patient.lastAppointment}</td>
+                    <td>{patient.objective}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </main>
+        <aside>
+          <CircleUser
+            width={120}
+            height={120}
+            strokeWidth={1}
+            color='#9ca3af'
+          />
+          <p>Selectiona un paciente para ver la información previa</p>
+        </aside>
       </section>
       {isModalOpen &&
         createPortal(
