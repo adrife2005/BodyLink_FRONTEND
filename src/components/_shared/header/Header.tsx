17fb70a5d44b533179bce56085f4178b710bdf9d
AdrifeCova <adrife2000@hotmail.com>
2025-02-13 10:36:46 -0600

    fix(header): removed path and added isModifying

--- src/components/_shared/header/Header.tsx
+++ src/components/_shared/header/Header.tsx
@@ -1,18 +1,12 @@
 import { CustomButton } from '@/components/ui/button/CustomButton'
 import { btnPrimaryStyles } from '@/components/ui/button/customStyles/buttonStyles'
-import { Sun, Bell, ChevronDown, ChevronRight } from 'lucide-react'
+import { Sun, Bell, ChevronDown } from 'lucide-react'
 import styles from './Header.module.css'
-import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
 
 export function Header() {
-  const location = useLocation()
   const [usingDarkMode, setUsingDarkMode] = useState(true)
-  const locationName = location.pathname.replace('/', '')
-  const formatingLocation = locationName
-    .charAt(0)
-    .toUpperCase()
-    .concat(locationName.slice(1))
+  const isModifying = false
 
   const toggleDarkMode = () => {
     document.body.className = usingDarkMode ? 'light' : 'dark'
@@ -21,11 +15,7 @@ export function Header() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        {/* This will change */}
-        Inicio <ChevronRight aria-label='arrow' />
-        {locationName && `${formatingLocation}`}
-      </div>
+      <div className={styles.left}>{isModifying && 'Guardado en la nube'}</div>
       <div className={styles.right}>
         <Sun aria-label='toggleDarkMode' onClick={toggleDarkMode} />
         <Bell aria-label='notifications' />
